1d0170d74242b36578aee0c831fc239c50534f70
Santiago Orozco Sanchez <sos@mbp-de-santiago.casa.lan>
2020-11-25 21:47:28 -0600

    add basic table response

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const
     tabla = require('./tablaPeriodica.json'),
     PORT = process.env.PORT || 5500;
 
+//config
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "pug");
+
+//pages
+app.get('/', (req, res) => {
+    res.render("index",{tabla:tabla.elements});
+});
 
 // Listen on port ...
 app.listen(PORT, () => {
