ac91bc76a861ff785fe23606647bad7a4cb06f3a
Santiago Orozco Sanchez <sos@mbp-de-santiago.casa.lan>
2020-12-03 18:31:08 -0600

    por fin se ve bien, no small commit

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
 const 
-    express = require("express"),
-    app = express(),
-    tabla = require('./tablaPeriodica.json'),
-    PORT = process.env.PORT || 5500;
+    express =   require("express"),
+    app =       express(),
+    tabla =     require('./tablaPeriodica.json'),
+    fs =        require('fs'),
+    pug=        require("pug"),
+    PORT =      process.env.PORT || 5500;
 
 //config
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public", {extensions:['html']}));
 app.set("view engine", "pug");
 
+//options
+let options = {
+    pretty:true,
+    pageName: "Equipo 11"
+}
+
 //gets
-app.get('/tabla', (req, res) => {
-    res.render("index",{tabla:tabla.elements, title:"Tabla Periodica"});
+app.get("/", (req, res) => {
+    options.title = "Pagina Principal"
+    res.render("index",options);
+    fs.writeFile("public/html/index.html", 
+        pug.renderFile("views/index.pug", options), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+}).get('/tabla', (req, res) => {
+    options.title = "Tabla Periodica";
+    options.tabla = tabla.elements;
+    res.render("tabla", options);
+    fs.writeFile("public/html/tabla.html", 
+        pug.renderFile("views/tabla.pug", options), (err) => {
+            if (err) { 
+                console.log(err);
+            }
+        })
+}).get("/PrimerParcial", (req, res) => {
+    options.title = "Primer parcial";
+    res.render("primerparcial", options);
+    fs.writeFile("public/html/primerparcial.html", 
+        pug.renderFile("views/primerparcial.pug", options), (err) => {
+            if (err) { 
+                console.log(err);
+            }
+        })
+}).get("/SegundoParcial", (req, res) => {
+    options.title = "Segundo Parcial";
+    res.render("segundoparcial", options);
+    fs.writeFile("public/html/segundoparcial.html", 
+        pug.renderFile("views/segundoparcial.pug", options), (err) => {
+            if (err) { 
+                console.log(err);
+            }
+        })
 })
 
 // Listen on port ...
 app.listen(PORT, () => {
-    console.log("Andamos Arriba!");
+    console.log(`Online on port: ${PORT}`);
 });
